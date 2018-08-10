78006205a327dc0b39deecdb1a24eca8cbcf4798
yangjing <yangjingpuyu@aliyun.com>
2018-08-10 15:48:11 +0800

    modify config

---
+++ src/views/home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+    Home
+    <button type="button" name="button">click me !</button>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped lang="stylus">
+</style>
