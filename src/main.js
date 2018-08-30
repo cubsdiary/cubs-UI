6b65da612726458ad545517bbf8681cffb73dd71
yangjing <yangjingpuyu@aliyun.com>
2018-08-30 23:35:45 +0800

    tabs

--- src/main.js
+++ src/main.js
@@ -4,10 +4,12 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import Notification from '@/components/notification'
+import Tabs from '@/components/tabs'
 import '@/common/stylus/reset.styl'
 Vue.config.productionTip = false
 
 Vue.use(Notification)
+Vue.use(Tabs)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
--- src/views/home.vue
+++ src/views/home.vue
@@ -2,18 +2,40 @@
   <div class="home">
     Home
     <button type="button" name="button" @click="showNotify">click me !</button>
-    <Notification content="test notify"/>
+    <!-- <Notification content="test notify"/> -->
+    <tabs :value="tabValue" @change="handleChangeTab">
+      <tab label="tab1" index="1">
+        <span>tab content 1</span>
+      </tab>
+      <tab index="2">
+        <span slot="label" style="color:red;">tab2</span>
+        <span>tab content 2</span>
+      </tab>
+      <tab label="tab3" index="3">
+        <span>tab content 3</span>
+      </tab>
+    </tabs>
   </div>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      tabValue: '1'
+    }
+  },
+  mounted () {
+  },
   methods: {
     showNotify () {
       this.$notify({
         content: 'test $notify',
         btn: 'close'
       })
+    },
+    handleChangeTab (index) {
+      this.tabValue = index
     }
   }
 }
